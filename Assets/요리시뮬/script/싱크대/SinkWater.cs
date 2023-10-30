b30adff4918743e59a0890ff1619197547bebb06
Shell4026 <rkddkwlenfdl@naver.com>
2023-10-30 09:29:12 +0900

    feat: 싱크대 물 틀 때 소리 추가

--- Assets/요리시뮬/script/싱크대/SinkWater.cs
+++ Assets/요리시뮬/script/싱크대/SinkWater.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class SinkWater : MonoBehaviour
 {
+    public WaterSound water_sound;
     Animator anim;
     void Start()
     {
@@ -13,9 +15,42 @@ public class SinkWater : MonoBehaviour
     public void On()
     {
         anim.SetInteger("state", 1);
+        if (water_sound != null)
+        {
+            water_sound.gameObject.SetActive(true);
+            water_sound.On();
+        }
     }
     public void Off()
     {
         anim.SetInteger("state", 2);
+        if (water_sound != null)
+            water_sound.Off();
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(SinkWater))]
+public class SinkWaterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        this.DrawDefaultInspector();
+
+        GUILayout.Space(10.0f);
+        GUILayout.Label("Å×½ºÆ®");
+        GUI.enabled = Application.isPlaying;
+        if(GUILayout.Button("On"))
+        {
+            SinkWater s = (SinkWater)target;
+            s.On();
+        }
+        if (GUILayout.Button("Off"))
+        {
+            SinkWater s = (SinkWater)target;
+            s.Off();
+        }
+        GUI.enabled = true;
+    }
+}
+#endif
---
+++ Assets/요리시뮬/script/싱크대/WaterSound.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class WaterSound : MonoBehaviour
+{
+    private AudioSource audios;
+
+    bool off = false;
+    void Start()
+    {
+        audios = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        audios = GetComponent<AudioSource>();
+        audios.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!off)
+        {
+            if (audios.isPlaying)
+            {
+                if (audios.time > 10.0f)
+                    audios.time -= 5.0f;
+            }
+        }
+        else
+        {
+            if(!audios.isPlaying)
+            {
+                off = false;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void On()
+    {
+        off = false;
+        audios.time = 0.0f;
+        audios.Play();
+    }
+
+    public void Off()
+    {
+        off = true;
+        audios.time = 96.5f;
+    }
+}
