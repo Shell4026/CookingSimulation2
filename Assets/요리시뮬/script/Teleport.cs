d1f7f8dd30f1a0d4bfd48caee6ab65f6160e4f62
zerojae2 <108273223+zerojae2@users.noreply.github.com>
2023-10-31 10:49:00 +0900

    추가:: 물체가 플레이어 시야를 따라오는 코드
    물체가 플레이어 시야를 따라오는 코드 Teleport

--- Assets/요리시뮬/script/Teleport.cs
+++ Assets/요리시뮬/script/Teleport.cs
@@ -11,6 +11,7 @@ public class Teleport : MonoBehaviour
     {
         if (targetCamera != null)
         {
+            transform.rotation = Quaternion.Euler(0, targetCamera.localEulerAngles.y, 0);
             // 카메라의 위치를 따라가도록 설정
             transform.position = Vector3.Lerp(transform.position, targetCamera.position, followSpeed * Time.deltaTime);
         }
