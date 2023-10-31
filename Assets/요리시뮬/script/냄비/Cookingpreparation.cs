03a745b405c74c2f2bf194d4101a1a7584b98209
zerojae2 <108273223+zerojae2@users.noreply.github.com>
2023-10-31 10:43:27 +0900

    feat:: Cookingpreparation 코드 수정
    리펙토링

--- Assets/요리시뮬/script/냄비/Cookingpreparation.cs
+++ Assets/요리시뮬/script/냄비/Cookingpreparation.cs
@@ -7,15 +7,18 @@ public class Cookingpreparation : MonoBehaviour
     private ButtonInteraction inductionButton;
     private PotInteraction pot;
     public GameObject fire;
+    public GameObject potObject;
     public float LimitTime;
+    public float ftime;
 
-    private bool water = false;
-    private bool ingredients = false;  //식재료
+    private bool water = true;
+    private bool ingredients = true; //식재료
+    private bool finsh = true;
 
     void Start()
     {
         inductionButton = GameObject.Find("inductionButton").GetComponent<ButtonInteraction>();
-        pot = GetComponent<PotInteraction>();
+        pot = potObject.GetComponent<PotInteraction>();
     }
 
     // Update is called once per frame
@@ -32,36 +35,47 @@ public class Cookingpreparation : MonoBehaviour
             }
             else if (water == true)
             {
-                LimitTime -= Time.deltaTime;
-                Debug.Log(LimitTime);
-                if (LimitTime >= 4 && LimitTime <= 6) //제 시간 안에 클릭을 했는가?
+                if (ingredients == true) //제 시간 안에 클릭을 했는가?
                 {
-                    if (ingredients == true) //yes 재료를 넣는다
+                    LimitTime -= Time.deltaTime;
+                    Debug.Log(LimitTime);
+                    if (LimitTime >= 4 && LimitTime <= 6 && Input.GetKeyDown(KeyCode.K) && finsh == false) //yes 재료를 넣는다
                     {
+                        finsh = true;
                         //완성 알아서 보글보글 한 후 완성
-                        //5초 경과(ui로 인덕션 끄라는 메시지 띄우기)
-                        if (inductionButton.isButtonPressed == false)
-                        {
-                            //라면 옮기기로 이동
-                        }
-                        else if (inductionButton.isButtonPressed == true)
-                        {
-                            //불남
-                            fire.SetActive(true);
-                        }
+                        Debug.Log("완성");
+                    }
+                    else if (LimitTime < 0)
+                    {
+                        //no 탔는지 덜익었는지 ui로 띄운 후 재시작
+                        LimitTime = 10f;
+                        Debug.Log("다시");
+                        water = false;
+                        ingredients = false;
+                        finsh = false;
+                        pot.isPotOnInduction = false;
+                        inductionButton.isButtonPressed = false;
                     }
                 }
-                else
-                {
-                    //no 탔는지 덜익었는지 ui로 띄운 후 재시작
-                    LimitTime = 10f;
-                }
-
-
-
             }
         }
-
+        //5초 경과(ui로 인덕션 끄라는 메시지 띄우기)
+        if (finsh == true) 
+        {
+            ftime -= Time.deltaTime;
+            Debug.Log(ftime);
+            if (ftime > 0 && inductionButton.isButtonPressed == false) // 제한시간 내에 했고 버튼이 꺼져있는 상태가 되면
+            {
+                //라면 옮기기로 이동
+                Debug.Log("성공");
+                finsh = false;
+            }
+            else if (ftime < 0 && inductionButton.isButtonPressed == true) // 제한시간 넘기고 버튼이 켜져있는 상태가 되면
+            {
+                //불남
+                fire.SetActive(true);
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
