21ce8a8a06cdf60e38db68a531be517259a6a235
Shell4026 <rkddkwlenfdl@naver.com>
2023-10-31 12:20:09 +0900

    refactor: Telport.cs이름 변경, 코드 개선

---
+++ Assets/요리시뮬/script/UI/FollowingUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingUI : MonoBehaviour
+{
+    [Header("플레이어와의 거리")]
+    public float distance = 10.0f;
+    [Header("플레이어 카메라")]
+    public Transform player_cam;
+    [Header("따라오는 속도")]
+    public float max_speed = 3.0f;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector3 goal = player_cam.position + player_cam.forward * distance;
+        float goal_dis = (goal - transform.position).magnitude;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, player_cam.transform.rotation, goal_dis * max_speed * Time.deltaTime);
+        //transform.position += goal * max_speed * Time.deltaTime; //2번째 방법
+        transform.position = Vector3.Slerp(transform.position, goal, goal_dis * max_speed * Time.deltaTime);
+    }
+}
