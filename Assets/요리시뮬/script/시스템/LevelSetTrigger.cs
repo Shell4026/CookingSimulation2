3e15690f96c25e72c6969a7d832d8eacae7cb389
Shell4026 <rkddkwlenfdl@naver.com>
2023-11-01 04:19:41 +0900

    feat: 레벨 설정 트리거 기능 확장

--- Assets/요리시뮬/script/시스템/LevelSetTrigger.cs
+++ Assets/요리시뮬/script/시스템/LevelSetTrigger.cs
@@ -5,23 +5,78 @@ using UnityEngine;
 public class LevelSetTrigger : MonoBehaviour
 {
     public Cookingpreparation manager;
+    [Header("null이면 플레이어")]
+    public Transform target;
     public int require_level = -1;
     public int level;
     public bool once = true;
+    public bool is_trigger = true;
     private void OnTriggerEnter(Collider other)
     {
+        if (!is_trigger)
+            return;
+
         if (require_level < 0)
             return;
 
         if (manager.GetLevel() != require_level)
             return;
 
-        if(other.CompareTag("Player"))
+        if (target == null)
         {
-            manager.LevelStart(level);
-            if(once)
+            if (other.CompareTag("Player"))
             {
-                gameObject.SetActive(false);
+                manager.LevelStart(level);
+                if (once)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            if(other.transform == target || other.transform.parent == target)
+            {
+                manager.LevelStart(level);
+                if (once)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (is_trigger)
+            return;
+
+        if (require_level < 0)
+            return;
+
+        if (manager.GetLevel() != require_level)
+            return;
+
+        if (target == null)
+        {
+            if (collision.transform.CompareTag("Player"))
+            {
+                manager.LevelStart(level);
+                if (once)
+                {
+                    this.enabled = false;
+                }
+            }
+        }
+        else
+        {
+            if (collision.transform == target || collision.transform.parent == target)
+            {
+                manager.LevelStart(level);
+                if (once)
+                {
+                    this.enabled = false;
+                }
             }
         }
     }
