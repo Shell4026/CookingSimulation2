3e52f11af86c8b97745c780e4090ab05fe2a7a7b
Shell4026 <rkddkwlenfdl@naver.com>
2023-11-01 03:14:44 +0900

    feat: 냄비 물 기울어지면 흘림 - Pot.cs

--- Assets/요리시뮬/script/냄비/Pot.cs
+++ Assets/요리시뮬/script/냄비/Pot.cs
@@ -15,18 +15,34 @@ public class Pot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float dot = Vector3.Dot(transform.up, Vector3.up);
+        if(dot < 0.3f)
+        {
+            FillOut();
+        }
     }
 
     public void Fill()
     {
-        if(water_amount < 1.0f)
+        if (water_amount < 1.0f)
         {
             water_amount += 0.1f * Time.deltaTime;
             anim.SetFloat("water", water_amount);
         }
+        else
+            water_amount = 1.0f;
     }
 
+    public void FillOut()
+    {
+        if (water_amount > 0.0f)
+        {
+            water_amount -= 0.2f * Time.deltaTime;
+            anim.SetFloat("water", water_amount);
+        }
+        else
+            water_amount = 0.0f;
+    }
     public void SetWaterAmount(float amount)
     {
         if (amount > 1.0f)
